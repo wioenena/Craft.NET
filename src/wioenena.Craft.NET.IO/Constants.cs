e2289d06c36e4172e57f01da19b4166ed0d1d744
wioenena <wioenena@gmail.com>
2025-02-24 07:09:07 +0300

    Update SEGMENT_BITS constant to use correct value

--- src/wioenena.Craft.NET.IO/Constants.cs
+++ src/wioenena.Craft.NET.IO/Constants.cs
@@ -2,7 +2,7 @@ namespace wioenena.Craft.NET.IO;
 
 public static class Constants {
 #pragma warning disable CA1707 // Identifiers should not contain underscores
-    public const int SEGMENT_BITS = 0x07;
+    public const int SEGMENT_BITS = 0x7F;
     public const int CONTINUE_BIT = 0x80;
 #pragma warning restore CA1707 // Identifiers should not contain underscores
 }
