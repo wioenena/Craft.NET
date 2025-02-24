613fa258f65bdb8edbee74a94554318bfb2aea6c
wioenena <wioenena@gmail.com>
2025-02-24 05:28:53 +0300

    Add Constants class with segment and continue bit definitions

---
+++ src/wioenena.Craft.NET.IO/Constants.cs
@@ -0,0 +1,8 @@
+namespace wioenena.Craft.NET.IO;
+
+public static class Constants {
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+    public const int SEGMENT_BITS = 0x07;
+    public const int CONTINUE_BIT = 0x80;
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+}
